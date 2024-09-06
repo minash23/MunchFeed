976dc79b580be7a936a8d26f5c8d18759b648bd0
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2024-09-06 13:36:31 -0500

    koko test

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+          //testing
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
