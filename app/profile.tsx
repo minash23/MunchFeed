42583f7075dda732659c66ecd2cb290f847e94a2
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-09-10 14:38:23 -0500

    rigo testing

---
+++ app/profile.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                //testing
+            }}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    );
+}
